a1008533674484db853ed3f6f51566c4c5878bb1
boba_and_beer <jacky2wong@gmail.com>
2021-08-23 15:42:46 +1000

    add setup.py
    add setup.[y

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='audioclip',
+    version='1.0.0',
+    url='',
+    author='',
+    author_email='',
+    description='',
+    packages=find_packages(),    
+    install_requires=[],
+)
