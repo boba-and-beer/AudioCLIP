0182d68a8aeee4df268eb6840d25781de63d04e3
boba_and_beer <jacky2wong@gmail.com>
2021-08-23 15:45:25 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+install_reqs = parse_requirements("requirements.txt")
 
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
 setup(
     name='audioclip',
     version='1.0.0',
@@ -8,5 +13,5 @@ setup(
     author_email='',
     description='',
     packages=find_packages(),    
-    install_requires=[],
+    install_requires=reqs,
 )
